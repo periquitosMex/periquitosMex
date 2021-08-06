6306a3419e4f4370d6540a74ca629d252908f486
oscardavidmendoza <oscardavid.mendoza@icloud.com>
2021-08-05 20:23:26 -0500

    initial folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the main page</h1>
+    </div>
+  );
+}
+
+export default App;
